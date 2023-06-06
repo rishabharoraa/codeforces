efad71fa9b8939c0a70096ed112c019a225eaa38
rishabharoraa <rishabharora.ns@gmail.com>
2023-06-07 00:50:55 +0530

    339A

--- 339A.cpp
+++ 339A.cpp
@@ -2,5 +2,16 @@
 using namespace std;
 
 int main() {
-	
+	string s; cin >> s;
+	vector<char> v;
+	for(char c: s) {
+		if(c == '+') continue;
+		v.push_back(c);
+	}
+	sort(v.begin(),v.end());
+	string res = "";
+	for(char c: v) {
+		res = res + c + "+";
+	}
+	cout << res.substr(0,res.size()-1) << endl;
 }
