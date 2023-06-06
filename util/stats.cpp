b4acdede487bff4fdfb13dfcd3f67f1d0d193a1f
rishabharoraa <rishabharora.ns@gmail.com>
2023-06-07 01:45:38 +0530

    updated stat util

--- util/stats.cpp
+++ util/stats.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 #include <map>
 #include <filesystem>
+#include <cmath>
+#include <iomanip>
 
 namespace fs = std::filesystem;
 
 int main() {
 	std::map<char,int> stats;
 	int total = 0;
+
 	for(const auto& entry : fs::directory_iterator("../")) {
 		std::string filename = entry.path();
 		if(filename.substr(filename.size() - 4) != ".cpp") continue;
@@ -18,8 +21,23 @@ int main() {
 		}
 		total++;
 	}
+
+	std::stringstream stream;
 	for(const std::pair<int,int>& p: stats) {
-		std::cout << (char)p.first << " -> " << p.second << std::string(5-(std::to_string(p.second)).size(), ' ') << "| " << (p.second/(double)total)*100 << "%"  << std::endl;
+		stream.str("");
+
+		float percentage = round(p.second/(double)total * 10000.0)/100;
+		stream << std::fixed << std::setprecision(2) << percentage;
+		std::string percentage_s = stream.str();
+
+		std::cout << 
+		(char)p.first << " ->" << 
+		std::string(4-(std::to_string(p.second)).size(), ' ') << p.second <<
+		"  |" << 
+		std::string(7-percentage_s.size(), ' ') <<
+		percentage_s << "%" << 
+		std::endl;
 	}
+
 	std::cout << total << " total problems solved" << std::endl;
 }
