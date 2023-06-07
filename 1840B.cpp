97b7dd6f9695722f6b706978177b8e8265bd6d72
rishabharora123 <rarora2@mphrx.com>
2023-06-07 08:16:43 +0530

    1840B

--- 1840B.cpp
+++ 1840B.cpp
@@ -1,18 +1,13 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <bitset>
+#include <cmath>
+#include <algorithm>
 using namespace std;
 
 void solve() {
 	int n, k; cin >> n >> k;
-	string s = bitset<128>(n).to_string();
-	s = s.substr(s.size() - k);
-	int x = 0;
-	for(char c: s) {
-		if(c == '1') {
-			break;
-		} x++;
-	}
-	int res = s.size() - x;
-	cout << pow(2, res) + 1 << endl;
+	k = min(k, 30);
+	cout << min(n, (1 << k) - 1) + 1 << endl;
 }
 
 int main() {
